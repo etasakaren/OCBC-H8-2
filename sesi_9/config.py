9d903d634b0a7e8828f29d4925898748029a8043
Etasa K <ekarenisa@yahoo.co.id>
2021-11-15 11:54:16 +0700

    deployed sesi-9

--- sesi_9/config.py
+++ sesi_9/config.py
@@ -13,8 +13,9 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'people.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+#     os.path.join(basedir, 'people.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dkdxjfunentlwj:a1d5d62b55be51540d06146e76dd8cf2914c71d51a90f3c6a76d345ba58c61fa@ec2-52-4-100-65.compute-1.amazonaws.com:5432/d3u34673am46op'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
