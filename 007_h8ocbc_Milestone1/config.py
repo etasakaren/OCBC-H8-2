08ab80de3e2c6c8151313be5d747a93e8f31d2d4
Etasa K <ekarenisa@yahoo.co.id>
2021-11-16 21:18:23 +0700

    db belum deployed

--- 007_h8ocbc_Milestone1/config.py
+++ 007_h8ocbc_Milestone1/config.py
@@ -13,7 +13,7 @@ app = connex_app.app
 
 # Configure the SQLAlchemy part of the app instance
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'final_proj.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tdzyhkkmmuiikk:03533ae5e8a1d99892f119c6e5839ec834aed13caef5f39aa0ed29ebd0491068@ec2-23-23-133-10.compute-1.amazonaws.com:5432/de4l3ht079kmtn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
